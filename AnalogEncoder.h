3bc356659ef103dbde0b465c49b04a1b5e6ab666
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-07-12 17:20:59 +0300

    Update AnalogEncoder.h

--- AnalogEncoder.h
+++ AnalogEncoder.h
@@ -1,10 +1,12 @@
 #include "fifo.h"
 #include "timer.h"
 
+enum class MOVEMENT_STATE {NONE, LEFT, RIGHT};
+
 class AnalogEncoder {
     public:
-        AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 10, uint16_t samplingRate_ms = 10);
-        int32_t read (); // Here runs the main integrating & comparison staff
+        AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 40, uint16_t samplingRate_ms = 10);
+        int32_t read (); // Insert this function in loop(). Here runs the main integrating & comparison staff
         inline void write (int32_t p) {position = p;};
         bool registerPattern (Fifo *pattern, CallBackFunction func); // Pattern format: Direction (+1/-1) / delayMs / Direction / delayMs / ...
 
@@ -13,6 +15,7 @@ class AnalogEncoder {
         Fifo *bufferR;
         Timer timer;
         int32_t position;
+        MOVEMENT_STATE movementState;
 }
 
 
@@ -21,17 +24,60 @@ class AnalogEncoder {
 AnalogEncoder::AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize, uint16_t samplingRate_ms) {
     pinMode (pinL, INPUT);
     pinMode (pinR, INPUT);
-    position = 0;
+
     bufferL = new Fifo (bufferSize);
     bufferR = new Fifo (bufferSize);
+    
     timer.setInterval (samplingRate_ms);
     timer.switchOn ();
+    
+    position = 0;
+    movementState = MOVEMENT_STATE::NONE;
 }
 
 int32_t AnalogEncoder::read () { // Here runs the main integrating & comparison staff
     const int positionIncrement = 4;
+    const float triggerRatio = 2.0;
+    #define DEBUG
+    #ifdef DEBUG
+        static long count = 0;
+        #define log(msg) Serial.print(msg)
+        #define logln(msg) Serial.println(msg)
+    #else
+        #define log(msg)
+        #define logln(msg)
+    #endif
     
     if (timer.needToTrigger ()) {
+        bufferL->insert (analogRead (pinL));
+        bufferR->insert (analogRead (pinR));
         
+        float aL = bufferL->average ();
+        float aR = bufferR->average ();
+        logln (++count);
+        log (F("bufferL->average: ")); logln (aL);
+        log (F("bufferR->average: ")); logln (aR);
+        
+        if (aR/aL >= triggerRatio) { // Left = dark, Right = bright; Movement L->R
+            switch (movementState) {
+                case MOVEMENT_STATE::NONE:
+                case MOVEMENT_STATE::LEFT:
+                    movementState = MOVEMENT_STATE::RIGHT;
+                    position += positionIncrement;
+                    break;
+            }
+        } else if (aL/aR >= triggerRatio) { // Movement R->L
+            switch (movementState) {
+                case MOVEMENT_STATE::NONE:
+                case MOVEMENT_STATE::RIGHT:
+                    movementState = MOVEMENT_STATE::LEFT;
+                    position -= positionIncrement;
+                    break;
+            }
+        } else { // Left = Right; NO Movement
+            
+        }
+        log (F("position: ")); logln (position);
     }
+    return position;
 }
