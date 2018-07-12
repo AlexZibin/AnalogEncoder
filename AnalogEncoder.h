3daad516c8684c1abc00d477853e19c569bccff6
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-07-12 15:13:38 +0300

    Update AnalogEncoder.h

--- AnalogEncoder.h
+++ AnalogEncoder.h
@@ -3,14 +3,19 @@
 
 class AnalogEncoder {
     public:
-        AnalogEncoder (uint8_t pinL, uint8_t pinR, int bufferSize = 10);
+        AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 10, uint16_t samplingRate_ms = 10);
     private:
-        Fifo *buffer;
+        Fifo *bufferL;
+        Fifo *bufferR;
+        Timer timer;
 }
 
 
 ////////  AnalogEncoder.cpp:
 
-AnalogEncoder::AnalogEncoder (uint8_t pinL, uint8_t pinR, int bufferSize = 10) {
-    buffer = new Fifo (bufferSize);
+AnalogEncoder::AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize, uint16_t samplingRate_ms) {
+    bufferL = new Fifo (bufferSize);
+    bufferR = new Fifo (bufferSize);
+    timer.setInterval (samplingRate_ms);
+    timer.switchOn ();
 }
