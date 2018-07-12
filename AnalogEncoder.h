0562dc62c5a103f2f20ae09e9352f47f61d726eb
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-07-12 16:44:37 +0300

    Update AnalogEncoder.h

--- AnalogEncoder.h
+++ AnalogEncoder.h
@@ -4,8 +4,9 @@
 class AnalogEncoder {
     public:
         AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 10, uint16_t samplingRate_ms = 10);
-        int32_t read ();
+        int32_t read (); // Here runs the main integrating & comparison staff
         inline void write (int32_t p) {position = p;};
+        bool registerPattern (Fifo *pattern, CallBackFunction func); // Pattern format: Direction (+1/-1) / delayMs / Direction / delayMs / ...
 
     private:
         Fifo *bufferL;
@@ -18,9 +19,19 @@ class AnalogEncoder {
 ////////  AnalogEncoder.cpp:
 
 AnalogEncoder::AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize, uint16_t samplingRate_ms) {
+    pinMode (pinL, INPUT);
+    pinMode (pinR, INPUT);
     position = 0;
     bufferL = new Fifo (bufferSize);
     bufferR = new Fifo (bufferSize);
     timer.setInterval (samplingRate_ms);
     timer.switchOn ();
+}
+
+int32_t AnalogEncoder::read () { // Here runs the main integrating & comparison staff
+    const int positionIncrement = 4;
+    
+    if (timer.needToTrigger ()) {
+        
+    }
 }
