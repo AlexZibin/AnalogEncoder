a97ba7bb2da9e93dd6e7399679f1480a70256b12
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-07-13 18:00:14 +0300

    Update AnalogEncoder.h

--- AnalogEncoder.h
+++ AnalogEncoder.h
@@ -5,7 +5,7 @@ enum class MOVEMENT_STATE {NONE, LEFT, RIGHT};
 
 class AnalogEncoder {
     public:
-        AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 20, uint16_t samplingRate_ms = 10);
+        AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 10, uint16_t samplingRate_ms = 10, int _triggerThreshold = 50);
         int32_t read (); // Insert this function in loop(). Here runs the main integrating & comparison staff
         inline void write (int32_t p) {position = p;};
         bool registerPattern (Fifo *_pattern, CallBackFunction func); // Pattern format: {Reserved=0, Direction (+1/-1), delayMs[, Direction, delayMs, ...]}
@@ -17,6 +17,8 @@ class AnalogEncoder {
         Fifo *pattern;
         Timer timer;
         int32_t position;
+        int triggerThreshold;
+
         MOVEMENT_STATE movementState;
         
         int8_t movementPhase; /*
@@ -30,33 +32,34 @@ class AnalogEncoder {
           2   |    0       0                            |      2   |    0       0
           3   |    1       0                            |      3   |    0       1
           4=0 |    1       1                            |      4=0 |    1       1
-              refValue-pinL > 0                                 refValue-pinL < 0
-              refValue-pinR < 0                                 refValue-pinR > 0
+              refValue-pinL ? 0                                 refValue-pinL 
+              refValue-pinR ? 0                                 refValue-pinR
               
         2) Controlling with light: (buffer3->average == 0) (negative == true)
         Phase |  pinL    pinR                           |    Phase |  pinL    pinR
-          0   |    0       0                            |      0   |    0       0 == refValue
+          0   |    0       0                            |      0   |    0       0 
           5   |    1       0                            |      5   |    0       1
           6   |    1       1                            |      6   |    1       1
           7   |    0       1                            |      7   |    1       0
           8=0 |    0       0                            |      8=0 |    0       0
-              refValue-pinL < 0
-              refValue-pinR > 0
+              refValue-pinL 
+              refValue-pinR 
         */
 }
 
 
 ////////  AnalogEncoder.cpp:
 
-AnalogEncoder::AnalogEncoder (uint8_t?? pins_arduino_h? pinL, uint8_t pinR, uint8_t bufferSize, uint16_t samplingRate_ms) {
+AnalogEncoder::AnalogEncoder (uint8_t?? pins_arduino_h? pinL, uint8_t pinR, 
+                              uint8_t bufferSize, uint16_t samplingRate_ms, int _triggerThreshold ) {
     pinMode (pinL, INPUT);
     pinMode (pinR, INPUT);
 
     bufferL = new Fifo (bufferSize);
     bufferR = new Fifo (bufferSize);
     buffer3 = new Fifo (bufferSize*2);
-    bufferL->insert (0);
-    bufferR->insert (0);
+    bufferL->insert (500);
+    bufferR->insert (500);
     
     pattern = nullptr;
     
@@ -64,14 +67,14 @@ AnalogEncoder::AnalogEncoder (uint8_t?? pins_arduino_h? pinL, uint8_t pinR, uint
     timer.switchOn ();
     
     position = 0;
+    triggerThreshold = _triggerThreshold;
     movementState = MOVEMENT_STATE::NONE;
     movementPhase = 0;
 }
 
 int32_t AnalogEncoder::read () { // Insert this function in loop(). Here runs the main integrating & comparison staff
-    //const int positionIncrement = 4;
-    const int triggerRatio = 2;
-    int refValue;
+    static int refValue;
+    static int levelToReachBySlave;
     
     #define DEBUG
     #ifdef DEBUG
@@ -103,23 +106,27 @@ int32_t AnalogEncoder::read () { // Insert this function in loop(). Here runs th
         if (buffer3->full ()) {
             switch (movementState) {
                 case MOVEMENT_STATE::NONE:
-                    if (aR/aL >= triggerRatio) { // Left is darker than Right
-                        if (refValue/aL >= triggerRatio) { // Left is going down; Right is stable, i.e. SHADOW moves from L to R
-                            movementState = MOVEMENT_STATE::RIGHT;
-                            movementPhase = 1;
-                            ++position;
-                        } else if (aR/refValue >= triggerRatio) { // Right is going up; Left is stable, i.e. LIGHT moves from R to L
-                            movementState = MOVEMENT_STATE::LEFT;
-                            movementPhase = 5;
-                            --position;
-                        }
-                    } else if (aL/aR >= triggerRatio) { // // Right is darker than Left
+                    if (abs (refValue - aL) >= triggerThreshold) { // moves from L to R; aL=master, aR=slave
+                        movementState = MOVEMENT_STATE::RIGHT;
+                        movementPhase = 1;
+                        levelToReachBySlave = aL;
+                        ++position;
+                    } else if (abs (refValue - aR) >= triggerThreshold) { // moves from R to L
+                        movementState = MOVEMENT_STATE::LEFT;
+                        movementPhase = 1;
+                        levelToReachBySlave = aR;
+                        --position;
                     } 
                     break;
                 case MOVEMENT_STATE::RIGHT:
                     switch (movementPhase) {
                         case 1:
-                            как узнать, что оба - одинаковые, но тёмные? buffer3->average ()!
+                            if (abs (aR-levelToReachBySlave) * 5 >= triggerThreshold) {
+                                ++movementPhase;
+                                 ////\\\\
+                                ////  \\\\
+                                
+                            }
                             break;
                     }
                     break;
