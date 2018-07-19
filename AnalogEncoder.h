af0823595312b703305127116e4690c337541ca4
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-07-19 15:41:21 +0300

    Update AnalogEncoder.h

--- AnalogEncoder.h
+++ AnalogEncoder.h
@@ -3,14 +3,20 @@
 
 // Idea and parts of code from https://github.com/mathertel/RotaryEncoder/blob/master/RotaryEncoder.cpp
 
-//enum class MOVEMENT_STATE {NONE, LEFT, RIGHT};
+struct PatternCtlInfo {
+    bool passedCurrentStepTest;
+    Fifo *pattern; // Pattern format: {Direction (+1/-1), delayMs[, Direction, delayMs, ...]}
+    void (*callBackFunction) (long);
+};
+
+PatternCtlInfo patternCtlInfo [] = {{0, nullptr, nullptr}, {0, nullptr, nullptr}, {0, nullptr, nullptr}};
 
 class AnalogEncoder {
     public:
         AnalogEncoder (uint8_t pinL, uint8_t pinR, uint8_t bufferSize = 10, uint16_t samplingRate_ms = 10, int _triggerThreshold = 50);
         int32_t read (void); // Insert this function in loop(). Here runs the main integrating & comparison staff
         inline void write (int32_t p) {position = p;};
-        bool registerPattern (Fifo *_pattern, CallBackFunction func); // Pattern format: {Reserved=0, Direction (+1/-1), delayMs[, Direction, delayMs, ...]}
+        //bool registerPattern (Fifo *_pattern, CallBackFunction func); // Pattern format: {Reserved=0, Direction (+1/-1), delayMs[, Direction, delayMs, ...]}
 
     private:
         Fifo *bufferL;
